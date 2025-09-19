a364b5cb7181713a7459462f6747826bc09568c1
TsunamiSesen <92399199+TsunamiSesen@users.noreply.github.com>
2025-09-18 22:20:26 -0600

    Create 00150.0.asm

---
+++ 00150.0.asm
@@ -0,0 +1,34 @@
+//Copy them in to the nodes as needed.  Top row will be 0 to 3, second 4 to 7, third 8 to 11
+//Direct fast approach
+
+@0
+MOV UP, DOWN
+
+@1
+
+@2
+MOV RIGHT, DOWN
+
+@3
+MOV UP, LEFT
+
+@4
+MOV UP, DOWN
+
+@5
+
+@6
+MOV UP, DOWN
+
+@7
+
+@8
+MOV UP, DOWN
+
+@9
+
+@10
+MOV UP, RIGHT
+
+@11
+MOV LEFT, DOWN
